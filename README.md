113539a74a5477f2d0cee1691702df7366cf40d1
Francisco Jose da Silva Sousa Junior <fco.silva.dev@gmail.com>
2023-03-06 20:54:50 +0300

    Add readme

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
 # Memory-Game
-Criação de um jogo de memória utilizando apenas HTML, CSS e Javascript
 
-Com poucas linhas de código, está sendo trabalhado efeitos 3D no CSS e lógica de programação utilizando condicionais, Immediately Invoked Function Expression e manipulação de Array em um projeto super divertido.
+Creation of a memory game using only HTML, CSS, and Javascript.
+
+With just a few lines of code, we've added 3D effects using CSS and implemented programming logic using conditionals, Immediately Invoked Function Expressions, and Array manipulation in a super fun project.
+
+## Installation
+
+1. Clone the repository: `git clone https://github.com/Burntroll/memory-game.git`
+2. Navigate to the project directory: `cd memory-game`
+3. Open the index.html file in your preferred browser.
+
+Alternatively, you can play the game online by visiting the following link: <Im going to host on a website soon>
+
+## Usage
+
+This game was created for entertainment and to test your memory skills. The objective of the game is to match pairs of cards with the same image. Each turn, the player selects two cards and if they match, the cards stay face up. If they do not match, the cards are flipped back over and the player continues until all cards are matched.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+
+## Acknowledgements
+
+- This game was inspired by classic memory games.
+
+## License
+
+This project is licensed under the [MIT License](https://opensource.org/licenses/MIT).
+
+## Results
+
+![gameplay-image](appweb.png)
--- script.js
+++ script.js
@@ -1,70 +1,68 @@
-const cards = document.querySelectorAll('.card');
+const cards = document.querySelectorAll(".card");
 let hasFlippedCard = false;
 let firstCard, secondCard;
 let lockBoard = false;
-
 
 /*
 function flipCard() {
     this.classList.toggle('flip')
 }
 Essa função funciona quantas vezes forem necessárias, virando e desvirando as cartas
-*/ 
+*/
 
 function flipCard() {
-    if(lockBoard) return;
-    if(this === firstCard) return;
-    this.classList.add('flip');
-    if(!hasFlippedCard) {
-        hasFlippedCard = true;
-        firstCard = this;
-        return;
-    }
+  if (lockBoard) return;
+  if (this === firstCard) return;
+  this.classList.add("flip");
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
 
-    secondCard = this;
-    hasFlippedCard = false;
-    checkForMatch();
+  secondCard = this;
+  hasFlippedCard = false;
+  checkForMatch();
 }
 
 function checkForMatch() {
-    if(firstCard.dataset.card === secondCard.dataset.card) {
-        disableCards();
-        return;
-    }
+  if (firstCard.dataset.card === secondCard.dataset.card) {
+    disableCards();
+    return;
+  }
 
-    unflipCards();
+  unflipCards();
 }
 
 function disableCards() {
-    firstCard.removeEventListener('click', flipCard);
-    secondCard.removeEventListener('click', flipCard);
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
 
-    resetBoard();
+  resetBoard();
 }
 
 function unflipCards() {
-    lockBoard = true;
-    setTimeout(() => {
-        firstCard.classList.remove('flip');
-        secondCard.classList.remove('flip');
+  lockBoard = true;
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
 
-        resetBoard();
-    }, 1500)
+    resetBoard();
+  }, 1500);
 }
 
 function resetBoard() {
-    [hasFlippedCard, lockBoard] = [false, false];
-    [firstCard, secondCard] = [null, null];
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
 }
 
 (function shuffle() {
-    cards.forEach((card) => {
-        let randomPosition = Math.floor(Math.random() * 12);
-        card.style.order = randomPosition;
-    })
+  cards.forEach((card) => {
+    let randomPosition = Math.floor(Math.random() * 12);
+    card.style.order = randomPosition;
+  });
 })();
 
 cards.forEach((cards) => {
-    cards.addEventListener('click', flipCard)
-})
-
+  cards.addEventListener("click", flipCard);
+});
